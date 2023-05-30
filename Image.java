41542d5067ffc44c53d321146674d9c6d0a94b76
Tran Khang <124011634+vka77@users.noreply.github.com>
2023-05-30 19:23:37 +0700

    Update Image.java

--- Image.java
+++ Image.java
@@ -6,44 +6,42 @@ public class Image {
 	   int height; // in pixels
 	   String source; // nguon` cua anh
 	   String quality; // chat luong
-	   Image(int width, int height, 
-	                String source, String quality) {
+	   Image(int width, int height,  String source, String quality ) {	              
 	      this.width = width;
 	      this.height = height;
 	      this.source = source;
 	      this.quality = quality;
-
-	   	}
-	   /* isPortrait: co phai la chan dung hay k
+	   }
+	  
+	    /* isPortrait: co phai la chan dung hay k
 	    * example:
 	    * witdh:1080
 	    * height: 1920
 	    * output: true
 	    */
-	   
-	   boolean isPortrait() {
+	   	boolean isPortrait() {
 		return this.width <this.height;
 		   
-	   }
+	  	 }
 	   /* kich thuoc cua anh? (pixel)
 	    * example
 	    * height: 1080
 	    * width: 1902
 	    * output: 1920*1080= 2073600
 	    */
-	   int size() {
+	   	int size() {
 		   return this.width*this.height;
 		   
-	   }
+	  	 }
 	   /* so sanh anh? nao lon hon
 	    * example: 
 	    * Image1.size > Image2.size=> return true
 	    * 
 	    */
-	   boolean isLarger () {
+	   	boolean isLarger () {
 		   return size()>size();
 		   
-	   }
+	   	}
 	   /* same: xem anh? nay co giong anh? kia ko
 	    * example:
 	    * this.source = that.source
@@ -67,11 +65,8 @@ public class Image {
    			else if (size()>10000 && size() <=1000000)
    				return "Medium";
    			else 
-   				return "Large";
-			
-   			
-   			
-   		}			
+   				return "Large";		
+   			}			
    	}
    		 
    	
